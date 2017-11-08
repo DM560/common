d990a31c4296b22e35895aa712d76cceea07810b
Marco Chiarandini <marco@imada.sdu.dk>
2017-11-08 23:23:11 +0100

    worked at calculator

--- Examples/Calculator/src/main.cpp
+++ Examples/Calculator/src/main.cpp
@@ -1,77 +1,196 @@
 #include "../../std_lib_facilities.h"
 
 
+//------------------------------------------------------------------------------
+
 class Token {
 public:
-	char kind;
-	double value;
+    char kind;        // what kind of token
+    double value;     // for numbers: a value 
+    Token(char ch)    // make a Token from a char
+    :kind(ch), value(0) { }    
+    Token(char ch, double val)     // make a Token from a char and a double
+    :kind(ch), value(val) { }
 };
 
-double expression()	// read and evaluate: 1   1+2.5   1+2+3.14  etc.
+//------------------------------------------------------------------------------
+
+Token get_token()    // read a token from cin
 {
-  double left = term(); 		// get the Term
-  while (true) {
-    Token t = get_token();	// get the next token...
-    switch (t.kind) {	// ... and do the right thing with it
-      case '+':     left += term(); break;
-      case '-':     left -= term(); break;
-      default:      return left;	// return the value of the expression
+    char ch;
+    cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
+    
+    switch (ch) {
+        //not yet   case ';':    // for "print"
+        //not yet   case 'q':    // for "quit"
+        case '(': case ')': case '+': case '-': case '*': case '/': 
+        return Token(ch);        // let each character represent itself
+        case '.':
+        case '0': case '1': case '2': case '3': case '4':
+        case '5': case '6': case '7': case '8': case '9':
+        {    
+            cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;              // read a floating-point number
+            return Token('8',val);   // let '8' represent "a number"
+        }
+        default:
+        error("Bad token");
     }
-  }
 }
 
 
 
+/*
+class Token_stream {
+public:
+	// user interface:
+	Token get();		// get a Token
+	void putback(Token); // put a Token back into the Token_stream
+private:
+	// representation: not directly accessible to users:
+	bool full {false};	// is there a Token in the buffer?
+	Token buffer;   // here is where we keep a Token put back using putback()
+};
 
 
-double term()	// exactly like expression(), but for * and  /
+
+void Token_stream::putback(Token t)
 {
-  double left = primary(); 		// get the Primary
-  while (true) {
-    Token t = get_token();	// get the next Token
-    switch (t.kind) {	
-      case '*':     left *= primary();	break;
-      case '/':
-      {	double d = primary();
-	if  (d==0) error("divide by zero");
-	left /= d;
-	break;
-      }
-      default:	    return left;	// return the value
-    }
-  }
+	if (full) error("putback() into a full buffer");
+	buffer=t;
+	full=true;
 }
 
-double primary()	// Number or '(' Expression ')'
+
+Token Token_stream::get()	 // read a Token from the Token_stream
 {
-  Token t = get_token();
-  switch (t.kind) {
-    case '(':			       // handle '('expression ')'
-    {	double d = expression();
-      t = get_token();
-      if (t.kind != ')') error("')' expected");
-      return d;
+    // check if we already have a Token ready
+    if (full) { full=false; return buffer; }  
+    
+    char ch;
+    cin >> ch;  // note that >> skips whitespace (space, newline, tab, etc.)
+    
+    switch (ch) {
+        case '(': case ')': case ';': case 'q': 
+        case '+': case '-': case '*': case '/': 
+        return Token{ch};           // let each character represent itself
+        case '.': case '0': case '1': case '2': case '3': case '4': 
+        case '5': case '6': case '7': case '8': case '9':
+        {	cin.putback(ch);         // put digit back into the input stream
+            double val;
+            cin >> val;		// read a floating-point number
+            return Token{'8',val};	// let '8' represent "a number"
+        }
+        default:  error("Bad token");
     }
-    case '8':		// we use '8' to represent the ``kind'' of a number
-    return t.value;	// return the number's value
-    default:
-    error("primary expected");
-  }
+}
+*/
+
+
+//------------------------------------------------------------------------------
+
+double expression();  // read and evaluate a Expression
+
+//------------------------------------------------------------------------------
+
+double term();        // read and evaluate a Term
+
+//------------------------------------------------------------------------------
+
+double primary()     // read and evaluate a Primary
+{
+    Token t = get_token();
+    switch (t.kind) {
+        case '(':    // handle '(' expression ')'
+        {    
+            double d = expression();
+            t = get_token();
+            if (t.kind != ')') error("')' expected");
+            return d;
+        }
+        case '8':            // we use '8' to represent a number
+        return t.value;  // return the number's value
+        default:
+        error("primary expected");
+    }
 }
 
-int main()
-try {
-  while (cin)
-  cout << expression() << '\n';
-  keep_window_open();		// for some Windows versions
+
+
+//------------------------------------------------------------------------------
+
+int main() {
+    try {
+        while (cin)
+        cout << expression() << '\n';
+        keep_window_open("~0");
+    }
+    catch (exception& e) {
+        cerr << e.what() << endl;
+        keep_window_open ("~1");
+        return 1;
+    }
+    catch (...) {
+        cerr << "exception \n";
+        keep_window_open ("~2");
+        return 2;
+    }
+    return 0;
 }
-catch (runtime_error& e) {
-  cerr << e.what() << endl;
-  keep_window_open ();
-  return 1;
+
+
+
+
+//------------------------------------------------------------------------------
+
+double expression()
+{
+    double left = term();      // read and evaluate a Term
+    Token t = get_token();     // get the next token
+    while(true) {    
+        switch(t.kind) {
+            case '+':
+            left += term();    // evaluate Term and add
+            t = get_token();
+            break;
+            case '-':
+            left -= term();    // evaluate Term and subtract
+            t = get_token();
+            break;
+            default:
+            return left;       // finally: no more + or -: return the answer
+        }
+    }
 }
-catch (...) {
-  cerr << "exception \n";
-  keep_window_open ();
-  return 2;
+
+
+//------------------------------------------------------------------------------
+
+double term()
+{
+    double left = primary();
+    Token t = get_token();     // get the next token
+    
+    while(true) {
+        switch (t.kind) {
+            case '*':
+            left *= primary();
+            t = get_token();
+            break;
+            case '/':
+            {    
+                double d = primary();
+                if (d == 0) error("divide by zero");
+                left /= d; 
+                t = get_token();
+                break;
+            }
+            default: 
+            return left;
+        }
+    }
 }
+
+
+//------------------------------------------------------------------------------
--- README.md
+++ README.md
@@ -1,12 +1,6 @@
-# march
+# Common
 
-https://docs.microsoft.com/en-US/visualstudio/ide/getting-started-with-cpp-in-visual-studio
+This repository is readable by everybody and writeable by the owner.
+It is mainly used to broadcast code used showed in class.
 
-https://www.youtube.com/watch?v=zNANRs0nZfU
-
-
-https://docs.microsoft.com/en-us/visualstudio/ide/solutions-and-projects-in-visual-studio
-https://docs.microsoft.com/en-us/visualstudio/ide/develop-code-in-visual-studio-without-projects-or-solutions
-
-A project is contained, in a logical sense and in the file system, within a solution, which may contain one or more related projects, along with build information, Visual Studio window settings, and any miscellaneous files that aren't associated with a particular project. A solution is described by a text file (.sln) with its own unique format; it is generally not intended to be edited by hand. 
 
